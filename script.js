86299ae28d552c094da34e4d790c83ad9795a1d9
Erick Viana <erickvianamelo@outlook.com>
2021-10-15 11:49:49 -0300

    Atualizado o projeto utilazando template strings

--- script.js
+++ script.js
@@ -23,39 +23,39 @@ function media() {
     
     let media = (nota1 + nota2)/2
 
-    let showMeida = document.querySelector('.media-valor');
+    let showMedia = document.querySelector('.media-valor');
 
 
     if (media >= 9) {
         removeBg();
-        showMeida.innerHTML = 'Parabéns sua média é ' + media + '!';
-        showMeida.style.display = 'block'
-        document.querySelector('.lineA').style.backgroundColor = '#008000'
+        showMedia.innerHTML = `Parabéns sua média é: ${media} !`;
+        showMedia.style.display = 'block'
+        lineA.style.backgroundColor = '#008000'
     } else if (media >= 7 && media < 9) {
         removeBg();
-        showMeida.innerHTML = 'Parabéns sua média é ' + media + '!';
-        showMeida.style.display = 'block'
-        document.querySelector('.lineB').style.backgroundColor = '#32CD32'
+        showMedia.innerHTML = `Parabéns sua média é: ${media} !`;
+        showMedia.style.display = 'block'
+        lineB.style.backgroundColor = '#32CD32'
     } else if (media >= 5 && media < 7) {
         removeBg();
-        showMeida.innerHTML = media + '!';
-        showMeida.style.display = 'block'
-        document.querySelector('.lineC').style.backgroundColor = '#9ACD32'
+        showMedia.innerHTML = `${media} !`;
+        showMedia.style.display = 'block'
+        lineC.style.backgroundColor = '#9ACD32'
     } else if (media >= 3 && media < 5) {
         removeBg();
-        showMeida.innerHTML = 'Que pena sua média é ' + media;
-        showMeida.style.display = 'block'
-        document.querySelector('.lineD').style.backgroundColor = '#FFFF00'
+        showMedia.innerHTML = `Que pena, sua média é: ${media}`;
+        showMedia.style.display = 'block'
+        lineD.style.backgroundColor = '#FFFF00'
     } else if (media >= 1 && media < 3) {
         removeBg();
-        showMeida.innerHTML = 'Sinto muito mas sua média é ' + media + ' :(';
-        showMeida.style.display = 'block'
-        document.querySelector('.lineE').style.backgroundColor = '#FF8C00'
+        showMedia.innerHTML = `Sinto muito, mas sua média é: ${media} :(`;
+        showMedia.style.display = 'block'
+        lineE.style.backgroundColor = '#FF8C00'
     } else {
         removeBg();
-        showMeida.innerHTML = media +'<br/>'+' Infelizmente você está REPROVADO!';
-        showMeida.style.display = 'block'
-        document.querySelector('.lineF').style.backgroundColor = '#B22222'    
+        showMedia.innerHTML = `${media} <br/><br/> Infelizmente você está REPROVADO!`;
+        showMedia.style.display = 'block'
+        lineF.style.backgroundColor = '#B22222'    
     }
 }
 
